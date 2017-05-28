ba4896d73499fb051efdafbe538fd7003927c9bb
gilhyeon <honggh34@gmail.com>
2017-05-29 00:48:31 +0900

    Maria DB로 db설정 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
-insert into USER (username, password, fullname) VALUES ('gilhyeon', 'gilhyeon', 'Gilhyeon Hong');
+/* 이 데이터 스크립트가 비어 있으면 시작할때 오류가 발생한다. 주석만 있어도 마찬가지. */
+--insert into USER (username, password, fullname) VALUES ('gilhyeon', 'gilhyeon', 'Gilhyeon Hong');
+select '1' from dual;
